8ab3bec227823117249e4fa1c4725220abf340d5
marnixah <marnix@hagehoeve.com>
2020-11-21 15:27:18 +0100

    Added stages sketch

---
+++ main.py
@@ -0,0 +1,10 @@
+from splash import splash
+from moviepy.editor import *
+
+# Stage 1 generating still frame 
+
+# Stage 2 Overlaying trailers over still frame
+
+# Stage 3 generating still frame with current session poster
+
+# Stage 4 concatenating stage 2 + 3
