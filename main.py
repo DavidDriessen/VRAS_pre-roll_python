7be6a593c9b5cc19c7ca4b02502a08917eacdf29
marnixah <marnix@hagehoeve.com>
2020-11-22 17:39:07 +0100

    Uncommented ffmpeg line

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ splash().save_frame('./output/posters.png')
 
 # Stage 2 Overlaying trailers over still frame
 for trailer in glob.glob('Trailers/*.mp4'):
-    #subprocess.call(["ffmpeg", "-i", "./output/posters.png", "-i", "./{0}".format(trailer), "-filter_complex", "[1:0]scale=900:506,setsar=1[a];[0:0][a] overlay=1020:574", "-map", "1:a", "-shortest", "-y", "./output/{0}".format(trailer)])
+    subprocess.call(["ffmpeg", "-i", "./output/posters.png", "-i", "./{0}".format(trailer), "-filter_complex", "[1:0]scale=900:506,setsar=1[a];[0:0][a] overlay=1020:574", "-map", "1:a", "-shortest", "-y", "./output/{0}".format(trailer)])
     pass
 # Stage 3 generating still frame with current session poster
 for session in glob.glob('Sessions/*'):
