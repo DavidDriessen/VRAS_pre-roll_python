c3c49f97cb2fe419b6c5a3c0b1b3e39e819bd6e6
marnixah <marnix@hagehoeve.com>
2020-11-22 17:36:10 +0100

    Fixed stage 4

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import subprocess
 import glob
 from pathlib import Path
 from random import shuffle
+import os
 
 # Stage 1 generating still frame 
 splash().save_frame('./output/posters.png')
 
 # Stage 2 Overlaying trailers over still frame
 for trailer in glob.glob('Trailers/*.mp4'):
-    subprocess.call("ffmpeg -i ./output/posters.png -i ./{0} -filter_complex \"[1:0]scale=900:506,setsar=1[a];[0:0][a] overlay=1020:574\" -map 1:a -shortest -y ./output/{0}".format(trailer))
+    #subprocess.call(["ffmpeg", "-i", "./output/posters.png", "-i", "./{0}".format(trailer), "-filter_complex", "[1:0]scale=900:506,setsar=1[a];[0:0][a] overlay=1020:574", "-map", "1:a", "-shortest", "-y", "./output/{0}".format(trailer)])
     pass
 # Stage 3 generating still frame with current session poster
 for session in glob.glob('Sessions/*'):
@@ -43,6 +44,10 @@ for splash in glob.glob("./output/splash/*"):
     p = Path(splash)
     current_session = p.stem.split("+")
 
+    cmd = ["ffmpeg", "-loop", "1", "-i", os.path.realpath(splash), "-c:v", "libx264", "-t", "10", "-pix_fmt", "yuv420p", "-vf", "scale=1920:1080", "-y", "./output/tmp/splash.mp4"]
+    p = subprocess.Popen(cmd)
+    p.wait()
+
     g = glob.glob("./output/Trailers/*")
     shuffle(g)
     for trailer in g:
@@ -51,9 +56,11 @@ for splash in glob.glob("./output/splash/*"):
         # Don't want to show a trailer of the show that we're currently showing
         if current_trailer in current_session:
             continue
-        concat_list.append(trailer)
+        concat_list.append( VideoFileClip( trailer.replace("\\","/") ) )
+        concat_list.append( VideoFileClip('./output/tmp/splash.mp4') )
+        
         pass
-    f = open("./concatlist.txt", "w")
-    f.write("file "+"\nfile ".join(concat_list))
-    f.close()
+    print(concat_list)
+    concat_clip = concatenate_videoclips(concat_list, method="compose") 
+    concat_clip.write_videofile("./output/00 {}.mp4".format("+".join(current_session)))
     pass
