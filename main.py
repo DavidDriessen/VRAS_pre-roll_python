b54b6df5ea5748447647f2124333ee83ebb531a5
marnixah <marnix@hagehoeve.com>
2020-12-24 22:50:36 +0100

    Changed all libraries called to a configurable path

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ from pathlib import Path
 from random import shuffle
 import os
 
+# Paths / prefixes
 font = "C:/WINDOWS/Fonts/SEGOEUI.TTF"
+magick_convert = ["magick","convert"]
+ffmpeg = ["ffmpeg"]
+ffprobe = ["ffprobe"]
 
 # Pre-run checks
 def check_dir(dir):
@@ -31,9 +35,10 @@ def check_program(cmd):
         sys.exit(cmd + " was not found on your system.")
         pass
     pass
-check_program("ffprobe")
-check_program("ffmpeg")
-check_program("magick")
+
+check_program(ffprobe[0])
+check_program(ffmpeg[0])
+check_program(magick_convert[0])
 
 if not os.path.isfile(font):
     sys.exit("Font " + font + " was not found on your system.")
@@ -61,7 +66,7 @@ if len(trailers) == 0:
     pass
 
 for trailer in trailers:
-    p = subprocess.Popen(["ffmpeg", "-i", "./output/posters.png", "-i", "./{0}".format(trailer), "-c:a","copy", "-filter_complex", "[1:0]scale=900:506,setsar=1[a];[0:0][a] overlay=1020:574", "-map", "1:a", "-shortest", "-y", "./output/{0}".format(trailer)])
+    p = subprocess.Popen(ffmpeg + ["-i", "./output/posters.png", "-i", "./{0}".format(trailer), "-c:a","copy", "-filter_complex", "[1:0]scale=900:506,setsar=1[a];[0:0][a] overlay=1020:574", "-map", "1:a", "-shortest", "-y", "./output/{0}".format(trailer)])
     p.wait()
     pass
 # Stage 3 generating still frame with current session poster
@@ -78,19 +83,19 @@ for session in sessions:
         name = p.stem
         current.append(name)
         poster_array.append("./output/tmp/{}.png".format(name))
-        cmd = ["magick","convert",poster,"-resize","320x450!","./output/tmp/{}.png".format(name)]
+        cmd = magick_convert + [poster,"-resize","320x450!","./output/tmp/{}.png".format(name)]
         p = subprocess.Popen(cmd)
         p.wait()
         pass
-    cmd = ["magick","convert"] + poster_array + ["+append","./output/tmp/lineup.png"]
+    cmd = magick_convert + poster_array + ["+append","./output/tmp/lineup.png"]
     p = subprocess.Popen(cmd)
     p.wait()
 
-    cmd = ["magick","convert","./output/posters.png","-gravity","center","-fill","white","-pointsize","40","-font",font,"-annotate","+480+40","This session:\n" + " & ".join(current), "./output/tmp/text.png"]
+    cmd = magick_convert + ["./output/posters.png","-gravity","center","-fill","white","-pointsize","40","-font",font,"-annotate","+480+40","This session:\n" + " & ".join(current), "./output/tmp/text.png"]
     p = subprocess.Popen(cmd)
     p.wait()
 
-    cmd = ["magick","convert","./output/tmp/text.png","./output/tmp/lineup.png", "-gravity","center","-geometry","+480+315","-composite", "./output/splash/{}.png".format("+".join(current))]
+    cmd = magick_convert + ["./output/tmp/text.png","./output/tmp/lineup.png", "-gravity","center","-geometry","+480+315","-composite", "./output/splash/{}.png".format("+".join(current))]
     p = subprocess.Popen(cmd)
     p.wait()
 # Stage 4 concatenating stage 2 + 3
@@ -100,7 +105,7 @@ for splash in glob.glob("./output/splash/*"):
     p = Path(splash)
     current_session = p.stem.split("+")
 
-    cmd = ["ffmpeg", "-f","lavfi", "-i", "anullsrc=channel_layout=stereo:sample_rate=44100", "-loop", "1", "-i", os.path.realpath(splash), "-c:v", "libx264", "-t", "20", "-pix_fmt", "yuv420p", "-vf", "scale=1920:1080", "-y", "./output/tmp/splash.mp4"]
+    cmd = ffmpeg + ["-f","lavfi", "-i", "anullsrc=channel_layout=stereo:sample_rate=44100", "-loop", "1", "-i", os.path.realpath(splash), "-c:v", "libx264", "-t", "20", "-pix_fmt", "yuv420p", "-vf", "scale=1920:1080", "-y", "./output/tmp/splash.mp4"]
     p = subprocess.Popen(cmd)
     p.wait()
 
@@ -120,7 +125,7 @@ for splash in glob.glob("./output/splash/*"):
     filter=""
     i=0
     length=0
-    cmd = ["ffmpeg"]
+    cmd = ffmpeg
     if len(concat_list) == 0:
         continue
     for video in concat_list:
@@ -128,7 +133,7 @@ for splash in glob.glob("./output/splash/*"):
         cmd.append("-i")
         cmd.append(video)
         
-        process = subprocess.Popen(["ffprobe", "-loglevel", "error", "-show_entries", "format=duration", "-of", "default=nw=1:nk=1", video],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process = subprocess.Popen(ffprobe + ["-loglevel", "error", "-show_entries", "format=duration", "-of", "default=nw=1:nk=1", video],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out = process.communicate()
         length = length + float(out[0])
 
